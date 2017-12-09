e3687b7063509834c6dea4f3fd00234b096f5a75
eric.wtlin <eric.wtlin@gmail.com>
2017-12-09 20:01:25 +0800

    add bar_stacked graph; add if/else example in latex algorithm

--- python-matplotlib/bar_stacked_compare.py
+++ python-matplotlib/bar_stacked_compare.py
@@ -8,10 +8,10 @@ from matplotlib.ticker import MultipleLocator, NullLocator
 from scipy import interpolate
 
 def make_model_compare_graph():
-    fig = plt.figure(figsize = [5, 3])    #[width, height]
+    fig = plt.figure(figsize = [5.5, 3])    #[width, height]
 
-    bar_width = 3
-    gap_bars_width = 1.3     #同组的bar之间的间隔
+    bar_width = 2.5
+    gap_bars_width = 1     #同组的bar之间的间隔
     gap_groups_width = 3    #不同组之间的间隔
     start = 1
     x1_data = []; x2_data = []; xticks_x = [];
@@ -22,21 +22,22 @@ def make_model_compare_graph():
         xticks_x.append(x1_data[i] + bar_width + gap_bars_width / 2)
         print(x1_data[i] + bar_width + gap_bars_width / 2)
 
-    xticks_label = ['Data1', 'Data2', 'Data3']
+    xticks_label = ['SL=0.01', 'SL=0.02', 'SL=0.03']
 
-    exp3_result = [92, 65, 78]
-    exp5_result = [91, 64, 77]
+    exp1_result = [0, 56.09, 12.82]
+    exp2_result = [58.66, 17.98, 8.47]
+    dbscan_time = [2.43] * len(exp2_result)
 
     #画第一幅图strict
-    title = "Strict Mode"
-    x_label = "Datasets"
-    y_label = "F$_1$-Score"
+    x_label = "Side Length"
+    y_label = "Computation Time"
     x_range = [0, x2_data[2] + bar_width + start]
-    y_range = [0, 120]
+    y_range = [0, 70]
     yticks_y = []
     yticks_label = []
-    legend2 = "Exp.3"
-    legend1 = "Exp.5"
+    legend1 = "Data1"
+    legend2 = "Data2"
+    legend3 = "Data3"
 
     font = {'family': 'Times New Roman', 'size': 12}
 
@@ -62,8 +63,9 @@ def make_model_compare_graph():
     else:
         plt.yticks(family='Times New Roman')
 
-    plt.bar(x1_data, exp3_result, bar_width, label=legend1, hatch='//', color='white', edgecolor='#F7819F')
-    plt.bar(x2_data, exp5_result, bar_width, label=legend2, hatch='.', color='white', edgecolor='#00BFFF')
+    plt.bar(x1_data, exp1_result, bar_width, label=legend1, hatch='//', color='white', edgecolor='#F7819F')
+    plt.bar(x2_data, exp2_result, bar_width, bottom=dbscan_time, label=legend2, hatch='.', color='white', edgecolor='#00BFFF')
+    plt.bar(x2_data, dbscan_time, bar_width, label=legend3, hatch='.', color='white', edgecolor='#81F781')
 
     font_legend = {'family': 'Times New Roman',
             'size': 11,
@@ -71,16 +73,18 @@ def make_model_compare_graph():
     plt.legend(loc='upper right',frameon=False, prop=font_legend)
 
     #在第一个条形上面添加y值
-    for x, y in zip(x1_data, exp3_result):
-        plt.text(x+bar_width/2, y+0.005, "%.2f" % y, ha='center', va='bottom', fontsize='11', fontdict=font)
+    for x, y in zip(x1_data, exp1_result):
+        if y == 0:
+            plt.text(x+bar_width/2, y+15, "Not appliable", rotation='vertical', ha='center', va='bottom', fontsize='11', fontdict={'family': 'Times New Roman', 'size': 12, 'color': '#F7819F'})
+        else:
+            plt.text(x+bar_width/2, y+0.005, "%.2f" % y, ha='center', va='bottom', fontsize='11', fontdict=font)
     #在第二个条形上添加y值
-    for x, y in zip(x2_data, exp5_result):
-        plt.text(x+bar_width/2, y+0.005, "%.2f" % y, ha='center', va='bottom', fontsize='11', fontdict=font)
+    for x, y, bottom in zip(x2_data, exp2_result, dbscan_time):
+        plt.text(x+bar_width/2, bottom + y+0.005, "%.2f" % y, ha='center', va='bottom', fontsize='11', fontdict=font)
 
     plt.tight_layout()
     #plt.show()
-    plt.savefig('bar_compare_single.png', dpi=300)
-    #plt.savefig('exp35.eps', format='eps', dpi=300)
+    plt.savefig('bar_stacked_compare.png', dpi=300)
 
 
 
