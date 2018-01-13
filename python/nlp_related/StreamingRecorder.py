4a6d80dd7411db7420386380a482e9970318ab34
eric.wtlin <eric.wtlin@gmail.com>
2018-01-13 23:04:50 +0800

    improve StreamingRecorder

--- python/nlp_related/StreamingRecorder.py
+++ python/nlp_related/StreamingRecorder.py
@@ -7,14 +7,39 @@ class StreamingRecorder():
         Args:
             names:  ['prediction', ... ]
         """
-        self.names = names
-        self.operators = dict()
-        self.recorder = dict()
+        self.__names = names
+        self.__operators = dict()
+        self.__recorder = dict()
         for name in names:
-            self.recorder[name] = []
+            self.__recorder[name] = []
 
     def record(self, name, values):
-        self.recorder[name].extend(values)
+        """ insert a col of multiple values
+
+        Args:
+            name:
+            values:
+
+        Returns:
+
+        """
+        if isinstance(values, list):
+            self.__recorder[name].extend(values)
+        else:
+            self.__recorder[name].append(values)
+
+    def record_one_row(self, values):
+        """ insert a whole row
+
+        Args:
+            values: [col1, col2, col3, ...], each element can be either a list or a single number
+
+        Returns:
+
+        """
+        assert len(self.__names) == len(values)
+        for name, value in zip(self.__names, values):
+            self.record(name, value)
 
     def get(self, name, operator=None):
         """
@@ -25,16 +50,28 @@ class StreamingRecorder():
                     None or 'origin': return the original values
                     'mean': return mean of the values
                     'sum': return sum of the values
+                    'min': return min of the values
+                    'max': return max of the values
 
         Returns:
 
         """
 
         if operator is None or operator == 'origin':
-            return self.recorder[name]
+            return self.__recorder[name]
         elif operator == 'mean':
-            return np.mean(self.recorder[name])
+            return np.mean(self.__recorder[name])
         elif operator == 'sum':
-            return np.sum(self.recorder[name])
+            return np.sum(self.__recorder[name])
+        elif operator == 'min':
+            return np.min(self.__recorder[name])
+        elif operator == 'max':
+            return np.max(self.__recorder[name])
 
+
+if __name__ == "__main__":
+    streaming_recorder = StreamingRecorder(['prediction'])
+    streaming_recorder.record('prediction', [1, 2, 3])
+    streaming_recorder.record('prediction', [4, 5, 6])
+    print(streaming_recorder.get('prediction', 'origin'))
 
