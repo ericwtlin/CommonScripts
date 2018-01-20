3b860530ba80899ec515aea4618992de8e3ee571
eric.wtlin <eric.wtlin@gmail.com>
2018-01-21 00:36:14 +0800

    add example of excel process by Pandas

--- python/excel_process.py
+++ python/excel_process.py
@@ -3,6 +3,17 @@ import pandas as pd
 import numpy as np
 
 
+def read_excel(excel_path):
+    sheets = pd.read_excel(excel_path, sheetname=None)      #get all sheets
+    print(sheets['sheet1'])
+
+    sheet = pd.read_excel(excel_path, sheetname="sheet1")   #get specific sheet
+
+    print(sheet['col_name'])
+
+    row_num = 5
+    print(sheet.get_value(row_num, 'col_name'))
+
 def write_excel():
     analysis_path = "test.xlsx"
     df = pd.DataFrame({
@@ -13,7 +24,13 @@ def write_excel():
         u'句子': np.arange(100),
         u'答案': np.arange(100)
     })
+
+    row_num = 5
+    df.set_value(row_num, '答案', "content to write")
+
     df = df.ix[:, [u'类别', u'备注', u'预测错', u'没预测出来', u'句子', u'答案']]  # 按指定列顺序排序
+
+
     excel_writer = pd.ExcelWriter(analysis_path)
     df.to_excel(excel_writer, 'Not Pred')
 
