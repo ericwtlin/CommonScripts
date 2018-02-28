338cc3e7c57b2bd75bd5e7a4ce629e794bf6061e
eric.wtlin <eric.wtlin@gmail.com>
2018-02-28 23:50:26 +0800

    update shell script

--- shell_scripts/batch_process.sh
+++ shell_scripts/batch_process.sh
@@ -1,21 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 PROCESS_FLAG="preprocess.py"
-MAX_PROCESS_NUM=3
+MAX_PROCESS_NUM=5
 
-index_begin=0
+index_begin=1
 index_end=5
 
 ###########AUTOMATION######################
 
 index=$index_begin
-while :
+while true
 do
-sleep 180	#detect every 180 seconds
 count=`ps -ef |grep $PROCESS_FLAG |grep -v "grep" |wc -l`
 if [ $count -lt $MAX_PROCESS_NUM ]
 then
-    nohup python preprocess.py corpus_${index}.0.txt pair/pair_${index}.txt &
-    index=`expr $index + 1`
+    run_time=0
+    while $run_time == 0
+    do
+        if [ ! -f "data/pair/pair_${index}.txt" ]; then
+            nohup python preprocess.py data/corpus_${index}.0.txt data/wvect.txt data/word2id.txt data/id2word.txt data/topfrequent.txt data/pair/pair_${index}.pkl &
+            run_time = `expr $run_time + 1`
+            #echo "preprocessing! $index"
+        fi
+        index=`expr $index + 1`
+    done
 fi
 
 if [ $index -gt $index_end ]
@@ -24,5 +31,6 @@ then
     break
 fi
 
-echo "Current working process num:${count}" 
+echo "Current working process num:${count}, the next index is: $index"
+sleep 3 #detect every 180 seconds
 done
---
+++ shell_scripts/common.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+echo "change the working directory to the parent dir of this shell script, but not where we activate the script"
+echo '$0: '$0
+echo `dirname $0`
+echo "pwd: "`pwd`
+echo "============================="
+echo "scriptPath1: "$(cd `dirname $0`; pwd)     #right
+echo "scriptPath2: "$(pwd)                      #wrong
+echo "scriptPath3: "$(dirname $(readlink -f $0))    #right
+echo "scriptPath4: "$(cd $(dirname ${BASH_SOURCE:-$0});pwd)     #right
+echo -n "scriptPath5: " && dirname $(readlink -f ${BASH_SOURCE[0]})     #right
+#thus, to change the working directory to the parent dir of this shell script:
+cd `dirname $0`
