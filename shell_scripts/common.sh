cb4a6481dd82ba151eea97978e483af7cd82fa56
eric.wtlin <eric.wtlin@gmail.com>
2018-03-10 19:58:39 +0800

    add simple  http server

--- shell_scripts/common.sh
+++ shell_scripts/common.sh
@@ -13,3 +13,8 @@ echo "scriptPath4: "$(cd $(dirname ${BASH_SOURCE:-$0});pwd)     #right
 echo -n "scriptPath5: " && dirname $(readlink -f ${BASH_SOURCE[0]})     #right
 #thus, to change the working directory to the parent dir of this shell script:
 cd `dirname $0`
+
+
+# printing time
+echo "`date +%c` hello world"
+echo `date +%c` "hello world"
