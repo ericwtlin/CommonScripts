d42e8bb8737e65ef776b5e6b095ae6fc7e68e265
Wutao Lin <eric.wtlin@gmail.com>
2018-09-03 16:56:23 +0800

    update sampling reservoid

--- python/SamplingReservoid.py
+++ python/SamplingReservoid.py
@@ -1,6 +1,6 @@
 #! -*- coding: utf-8 -*-
 import logging
-import cPickle as pkl
+import pickle as pkl
 import codecs
 import random
 import math
@@ -8,7 +8,7 @@ import os
 import time
 import re
 from random import randint
-
+import click
 
 class SamplingReservoid:
     def __init__(self, sample_num):
@@ -40,4 +40,23 @@ class SamplingReservoid:
             return False
         else:
             return True
+
+
+@click.command()
+@click.argument('input_path', type=click.STRING)
+@click.argument('output_path', type=click.STRING)
+@click.argument('sample_num', type=click.INT)
+def main(input_path, output_path, sample_num):
+    sampler = SamplingReservoid(sample_num)
+    with codecs.open(input_path, 'r', encoding='utf-8') as fin:
+        for line in fin:
+            sampler.add_sample(line)
+
+    with codecs.open(output_path, 'w', encoding='utf-8') as fout:
+        for line in sampler.get_sample_result():
+            fout.write(line)
+
+
+if __name__ == '__main__':
+    main()
 
