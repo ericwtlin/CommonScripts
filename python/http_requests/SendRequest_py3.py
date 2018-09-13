8996fa012cc0b3928dc91f697f14e96693ae63e4
Wutao Lin <eric.wtlin@gmail.com>
2018-09-13 20:24:36 +0800

    process http request with raw text body

--- python/http_requests/SendRequest_py3.py
+++ python/http_requests/SendRequest_py3.py
@@ -49,11 +49,7 @@ def one_time(url, json_data, method="GET"):
     headers = {'User-Agent': user_agent}
     if method == "GET":
         #solution 1
-        response = requests.get(url, headers=headers)
-
-        #solution 2 :   doesn't work yet
-        #headers = {'User-Agent': user_agent, "Content-Type": "text/html"}
-        #response = requests.get(url, json.dumps(json_data), headers=headers)
+        response = requests.get(url, json.dumps(json_data), headers=headers)
     else:
         #solution 1
         response = requests.post(url, json.dumps(json_data), headers=headers)
@@ -61,13 +57,33 @@ def one_time(url, json_data, method="GET"):
     print(response.text)
 
 
+def one_time_raw_text(url, plain_text, method="GET"):
+    """ one time request
+
+    :param url: e.g. 'http://127.0.0.1:8080/api/v1/addrecord/1'
+    :param plain_text:
+    :param method: GET/POST
+    :return:
+    """
+    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+    headers = {'User-Agent': user_agent}
+    if method == "GET":
+        response = requests.get(url, data=plain_text)
+    else:
+        response = requests.post(url, data=plain_text, headers=headers)
+    print(response.text)
+
 
 if __name__ == "__main__":
+    '''
     connect("127.0.0.1", 8889, "/", {'who': 'me'}, 'GET')
     connect("127.0.0.1", 8889, "/", {'who': 'me'}, 'POST')
     one_time('http://127.0.0.1:8889/', {'who': 'me'}, 'POST')
     one_time('http://127.0.0.1:8889/?who=me', 'GET')
-
+    
     one_time('http://127.0.0.1:8889/deal?website=baidu&city=beijing', 'GET')
+    '''
+
+    one_time_raw_text('http://127.0.0.1:8889/rawtext', 'I love you\t1', 'POST')
 
 
--- python/http_requests/TornadoServer.py
+++ python/http_requests/TornadoServer.py
@@ -43,12 +43,44 @@ class ExampleHandler(tornado.web.RequestHandler):
         else:
             self.write("POST: Hello World!")
 
+class RawTextHandler(tornado.web.RequestHandler):
+
+    def is_binary_content(self):
+        content_type = self.request.headers.get("Content-Type", "text/plain")
+        return (content_type == "application/binary")
+
+    def post(self):
+        if (not self.request.body):
+            self.set_status(500)
+            self.write("empty request")
+            self.finish()
+            return
+
+        try:
+            if self.is_binary_content():
+                response = self.request.body
+                self.set_header("Content-Type", "application/binary")
+                self.write(response)
+            else:
+                response = self.request.body.decode('utf-8')
+                req_split = self.request.body.decode('utf-8').split('\t')
+                print(req_split)
+                self.set_header("Content-Type", "text/plain")
+                self.write(response)
+
+            self.finish()
+            return
+        except Exception as e:
+            self.set_status(500)
+            self.write("internal server error: %s" % e)
+            self.finish()
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", ExampleHandler),
             (r"/deal", DealHandler),
+            (r"/rawtext", RawTextHandler),
         ]
         settings = dict()
         tornado.web.Application.__init__(self, handlers, settings)
@@ -58,8 +90,9 @@ def create_server():
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(options.port)
+    print('Server started...Listening on port %s' % str(options.port))
     tornado.ioloop.IOLoop.instance().start()
 
 
 if __name__ == "__main__":
-    create_server()
+    create_server()     # e.g. python TornadoServer.py --port=8080, or simply python TornadoServer.py
